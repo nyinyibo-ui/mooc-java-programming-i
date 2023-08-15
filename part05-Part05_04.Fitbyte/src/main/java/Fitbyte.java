3d5da9a446a0545454c9f6c333b6675f21e8a844
Nyi Bo <alexenderjunior6@gmail.com>
2023-08-15 18:23:28 +0630

    Submit Learning object-oriented programming

---
+++ part05-Part05_04.Fitbyte/src/main/java/Fitbyte.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alexe
+ */
+public class Fitbyte {
+    private int age;
+    private int restingHeartRate;
+    
+    public Fitbyte(int age,int restingHeartRate){
+        this.age=age;
+        this.restingHeartRate=restingHeartRate;
+    }
+    
+    public double targetHeartRate(double percentageOfMaximum){
+        double maxHeartRate=206.3-(.711*this.age);
+        double result=(maxHeartRate-this.restingHeartRate)*(percentageOfMaximum)+this.restingHeartRate;
+        return result;
+    }
+}
