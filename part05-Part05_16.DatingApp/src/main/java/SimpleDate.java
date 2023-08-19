36d1a1b0e167cdfcbbf4019f01b9df5440f3dcc5
Nyi Bo <alexenderjunior6@gmail.com>
2023-08-19 10:40:22 +0630

    modify objects and referenct

--- part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
+++ part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
@@ -25,12 +25,52 @@ public class SimpleDate {
             return true;
         }
 
-        if (this.year == compared.year && this.month == compared.month &&
-                 this.day < compared.day) {
+        if (this.year == compared.year && this.month == compared.month
+                && this.day < compared.day) {
             return true;
         }
 
         return false;
+    }
+    
+    //advance month
+    public void advanceMonth(int howManyMonths){
+        this.month+=howManyMonths;
+    }
+    
+    //advance year
+    public void advanceYear(int howManyYears){
+        this.year+=howManyYears;
+    }
+    
+    //advance day by number of days
+    public void advance(int howManyDays){
+        if (this.day+howManyDays<=30){
+            this.day+=howManyDays;
+        }else {
+            this.day=this.day+howManyDays-30;
+            if (this.month<12){
+                this.advanceMonth(1);
+            }else{
+                this.advanceYear(1);
+                this.month=0;
+                this.advanceMonth(1);
+            }
+        }
+    }
+    
+
+    //advance day by one day
+    public void advance() {
+        this.advance(1);
+    }
+    
+    public SimpleDate afterNumberOfDays(int days){        
+        SimpleDate newDate=new SimpleDate (this.day,this.month,this.year);
+        
+        newDate.advance(days);
+        
+        return newDate;
     }
 
 }
