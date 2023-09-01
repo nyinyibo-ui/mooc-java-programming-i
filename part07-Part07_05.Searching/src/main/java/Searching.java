2c918f5cc6d1584844e7f559332aecc2998b966a
Nyi Bo <alexenderjunior6@gmail.com>
2023-09-01 09:33:51 +0630

    updating searching

--- part07-Part07_05.Searching/src/main/java/Searching.java
+++ part07-Part07_05.Searching/src/main/java/Searching.java
@@ -1,6 +1,8 @@
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Searching {
 
@@ -42,13 +44,55 @@ public class Searching {
         }
 
     }
+    
+    // to you this function to sort the unordered arrayList of object
+public static ArrayList<Book> sortBooksById(ArrayList<Book> books) {
+    Collections.sort(books, new Comparator<Book>() {
+        public int compare(Book b1, Book b2) {
+            return Integer.compare(b1.getId(),b2.getId());
+        }
+    });
+    return books;
+}
 
     public static int linearSearch(ArrayList<Book> books, int searchedId) {
+
+        // if you wnat sort the id of books
+//        books=sortBooksById(books);
+
+        // find the searched ID in the id list
+        for (Book book : books) {
+            if (book.getId()== searchedId) {
+                return books.indexOf(book);
+            }
+        }
         return -1;
     }
 
     public static int binarySearch(ArrayList<Book> books, long searchedId) {
+
+        // if you want to sort the id of books
+//        books=sortBooksById(books);
+        
+        //set initial upper and lower limit
+        int upper = books.size() - 1;
+        int lower = 0;
+
+        while (lower <= upper) {
+            int middle = (upper + lower) / 2;
+            Book book=books.get(middle);
+            int id=book.getId();
+            if (id == searchedId) {
+                return middle;
+            }//if the value at list[middle] is smaller than searched
+            else if (id < searchedId) {
+                lower = middle + 1;
+            } //    if the value at list[middle] is larger than searched
+            else if (id> searchedId) {
+                upper = middle - 1;
+            }
+        }
+
         return -1;
     }
 }
-
